644fc4eca0710fdb047a9d3d02bc5fe7f83dad49
MahmoudAGaber <mrekaby@outlook.com>
2023-03-25 16:44:54 +0200

    added

--- supervised_learning.py
+++ supervised_learning.py
@@ -3,7 +3,7 @@
 
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import fetch_california_housing
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -19,7 +19,7 @@ print("\nClassification KNN model accuracy for the IRIS dataset")
 iris = load_iris()
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=98)
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=3)
 # test_size: proportion of test dataset to include in test (0 < x < 1)
 # random_state: integer that controls shuffling in dataset. Use an integer you know for reproducible output
 
@@ -32,16 +32,16 @@ accuracy = knn.score(X_test, y_test)
 print("Accuracy: {:.2f}%".format(accuracy*100))
 
 # Regression:
-# Project: Predicting the price of a house based on its features.
-# Number of dataset instances: 20640, 8 attributes with no missing attribute values
-# target is median house value expressed in hundreds of Thousands
-# https://inria.github.io/scikit-learn-mooc/python_scripts/datasets_california_housing.html
-# Load the California Housing dataset
+# Project: Providing a quantitative measure of disease progression one year after baseline.
+# Number of dataset instances: 442, 10 attributes with no missing attribute values
+# target is quantitative measure of disease progression one year after baseline
+# https://www4.stat.ncsu.edu/~boos/var.select/diabetes.html
+# Load the diabetes dataset
 #
-housing = fetch_california_housing()
+diabetes = load_diabetes()
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(housing.data, housing.target, test_size=0.7, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(diabetes.data, diabetes.target, test_size=0.7, random_state=42)
 # test_size: proportion of test dataset to include in test (0 < x < 1)
 # random_state: integer that controls shuffling in dataset. Use an integer you know for reproducible output
 
@@ -53,6 +53,6 @@ lr.fit(X_train, y_train)
 y_pred = lr.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
-print("Regression Model MSE & R2 Score for the California Housing Dataset")
+print("Regression Model MSE & R2 Score for the Diabetes Dataset")
 print("Mean squared error: {:.2f}".format(mse))
 print("R2 score: {:.2f}".format(r2))
